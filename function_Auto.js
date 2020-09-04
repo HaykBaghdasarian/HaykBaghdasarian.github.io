2e7ad77bd17acf5eeca7e2989dad4d80fe243965
HaykBaghdasarian <64759880+HaykBaghdasarian@users.noreply.github.com>
2020-09-04 17:59:32 +0300

    Update function_Auto.js

--- function_Auto.js
+++ function_Auto.js
@@ -1,5 +1,26 @@
 console.log("asdasddsa");
 
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 map.addControl(
 new mapboxgl.GeolocateControl({
 positionOptions: {
