4025b9d00441172856e30a115a7e3e0def4a0f1c
HaykBaghdasarian <64759880+HaykBaghdasarian@users.noreply.github.com>
2020-09-17 20:37:46 +0400

    Add files via upload

--- function.js
+++ function.js
@@ -1,23 +1,49 @@
 
 
+fetch("http://localhost/design/Data.json")
+.then(response => response.json())
+.then(data => {
+
+    data.forEach(data => {
+
+        var el = document.createElement('div');
+        el.className = data.type + '_marker';
+        el.id = data.id;
+        el.onclick = function () {
+            alert(data.id);
+        };
+        
+                
+        new mapboxgl.Marker(el)
+        .setLngLat([ data.longitude , data.latitude])
+        .addTo(map);
+
+    });
+});
+
+
+
+
+
 function change_image(id){
 
-    if(document.getElementById(id).src == "https://haykbaghdasarian.github.io/" + id + ".png"){
+    
+
+  if(document.getElementById(id).src == "https://haykbaghdasarian.github.io/" + id + ".png"){
+
    document.getElementById(id).src = "https://haykbaghdasarian.github.io/" + id + "_clicked.png";
+
+
+
+
+
+
 }else {
     document.getElementById(id).src = "https://haykbaghdasarian.github.io/" + id + ".png";
-}
-}
 
-// var layerList = document.getElementById('menu');
-// var inputs = layerList.getElementsByTagName('input');
- 
-// function switchLayer(layer) {
-// var layerId = layer.target.id;
-// map.setStyle('mapbox://styles/mapbox/' + layerId);
-// }
- 
-// for (var i = 0; i < inputs.length; i++) {
-// inputs[i].onclick = switchLayer;
-// }
+
+
+    
+}
+}
 
