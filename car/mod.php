96dd9deda91b5120a873497ed0f87121052c0524
HaykBaghdasarian <64759880+HaykBaghdasarian@users.noreply.github.com>
2023-08-05 04:25:10 +0300

    Delete car directory

--- car/mod.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if(isset($_POST['id'])){
-		$id = $_POST['id'];//mysqli_real_escape_string($con, $_POST['id']);
-		echo "<h1 style='color:#fff'>".$_POST['id']."</h1>";
-	}else{
-		echo "cka";
-	}
-?>
